f28e2d75b8f03b90d4c970169d479e873992e67c
Kousha Motazedian <k.d.Motazedian@Gmail.com>
2021-09-23 20:18:15 -0400

    For branched

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -18,6 +18,7 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        System.out.println("This is the branched one");
         
 
     }
